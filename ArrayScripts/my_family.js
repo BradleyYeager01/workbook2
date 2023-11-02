59a09726ccccb772345711503397e1666c176e01
Bradley Yeager <drifterdubz@gmail.com>
2023-11-02 10:58:50 -0400

    11/2/2023

--- ArrayScripts/my_family.js
+++ ArrayScripts/my_family.js
@@ -20,64 +20,82 @@
 // let average = total / score.length;
 // console.log(average);
 
-let courses = [
-  {
-    CourseId: "PROG100",
-    Title: "Introduction to HTML/CSS/Git",
-    Location: "Classroom 7",
-    StartDate: "09/08/22",
-    Fee: "100.00",
-  },
-  {
-    CourseId: "PROG200",
-    Title: "Introduction to JavaScript",
-    Location: "Classroom 9",
-    StartDate: "11/22/22",
-    Fee: "350.00",
-  },
-  {
-    CourseId: "PROG300",
-    Title: "Introduction to Java",
-    Location: "Classroom 1",
-    StartDate: "01/09/23",
-    Fee: "50.00",
-  },
-  {
-    CourseId: "PROG400",
-    Title: "Introduction to SQL and Databases",
-    Location: "Classroom 7",
-    StartDate: "03/16/23",
-    Fee: "50.00",
-  },
-  {
-    CourseId: "PROJ500",
-    Title: "Introduction to Angular",
-    Location: "Classroom 1",
-    StartDate: "04/25/23",
-    Fee: "50.00",
-  },
-];
+// let courses = [
+//   {
+//     CourseId: "PROG100",
+//     Title: "Introduction to HTML/CSS/Git",
+//     Location: "Classroom 7",
+//     StartDate: "09/08/22",
+//     Fee: "100.00",
+//   },
+//   {
+//     CourseId: "PROG200",
+//     Title: "Introduction to JavaScript",
+//     Location: "Classroom 9",
+//     StartDate: "11/22/22",
+//     Fee: "350.00",
+//   },
+//   {
+//     CourseId: "PROG300",
+//     Title: "Introduction to Java",
+//     Location: "Classroom 1",
+//     StartDate: "01/09/23",
+//     Fee: "50.00",
+//   },
+//   {
+//     CourseId: "PROG400",
+//     Title: "Introduction to SQL and Databases",
+//     Location: "Classroom 7",
+//     StartDate: "03/16/23",
+//     Fee: "50.00",
+//   },
+//   {
+//     CourseId: "PROJ500",
+//     Title: "Introduction to Angular",
+//     Location: "Classroom 1",
+//     StartDate: "04/25/23",
+//     Fee: "50.00",
+//   },
+// ];
 
-for (const course of courses) {
-  if (course.CourseId == "PROG200") {
-    console.log(course.StartDate);
-  }
-}
+// for (const course of courses) {
+//   if (course.CourseId == "PROG200") {
+//     console.log(course.StartDate);
+//   }
+// }
 
-for (const course of courses) {
-    if (course.CourseId == "PROJ500") {
-        console.log(course.Title);
+// for (const course of courses) {
+//     if (course.CourseId == "PROJ500") {
+//         console.log(course.Title);
+//     }
+// }
+
+// for (const course of courses) {
+//     if (course.Fee == "50.00") {
+//         console.log(course.Title);
+//     }
+// }
+
+// for (const course of courses) {
+//     if (course.Location == "Classroom 1") {
+//         console.log(course.Title);
+//     }
+//}
+
+let products = [
+    {product: "Gummy Worms", price: 5.79},
+    {product: "Plain M&Ms", price: 2.89},
+    {product: "Peanut M&Ms", price: 2.89},
+    {product: "Swedish Fish", price: 3.79},
+    // TODO: fill the array with 10 candies of various
+    // price ranges
+   ];
+
+   for (const candy of products) {
+    let foundMnMs = candy.product.indexOf("M&Ms") != -1;
+    if (foundMnMs) {
+        console.log(candy.product);
+         console.log(foundMnMs);
     }
-}
-
-for (const course of courses) {
-    if (course.Fee == "50.00") {
-        console.log(course.Title);
-    }
-}
-
-for (const course of courses) {
-    if (course.Location == "Classroom 1") {
-        console.log(course.Title);
-    }
-}
+    
+   }
